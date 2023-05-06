3721d0b2e0bf6a99012611885867168ada6a5690
Pranav50 <Pranav50>
2023-05-06 22:33:40 +0530

    small-change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar = ({handleSearchInput}) => {
     return (
         <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark justify-content-between">
-            <a className="navbar-brand" href="#">Players</a>
+            <a className="navbar-brand">Players</a>
             <form className="form-inline my-2 my-lg-0">
                 <input className="form-control mr-sm-2" type="search" placeholder="Search" onChange={(e) => handleSearchInput(e.target.value)} aria-label="Search" />
             </form>
